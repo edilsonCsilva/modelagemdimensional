e14038654855345b7f70e87bf082da399b6495cd
unknown <edilsonclaudinosilva@gmail.com>
2022-01-28 16:35:33 -0300

    Criação da Base de Dados De Teste parte2

--- gerador/DaoProducts.py
+++ gerador/DaoProducts.py
@@ -1,10 +1,13 @@
-from gerador.Product import Product
+
+from Product import Product
+
+
 products=[]
 products.append(
     Product("3252219113",
             "notebook compaq presario 420 intel pentium-n3700 4gb 120gb w10 14,1'' cinza",
             "INFORMATICA",
-            'https://images-submarino.b2w.io/Products/01/00/img/3252219/1/3252219121_1GG.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOGZIsUsLDdkF1yEZ8lNIiIwQSOI5All-KXhbGx7K4w_ErW1lv_LBUPG6YnL4JhOYPi6o&usqp=CAU',
             1499.99,"")
             
     )
@@ -14,7 +17,7 @@ products.append(
     Product("2810589401",
             "notebook gamer asus, intel coret i5 9300h, 8gb, 256gb ssd, 15,6 full hd 120hz, nvidia gtx 1650, preto - x571gt-al887t",
             "INFORMATICA",
-            'https://images-submarino.b2w.io/Products/01/00/img/3252219/1/3252219121_1GG.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOGZIsUsLDdkF1yEZ8lNIiIwQSOI5All-KXhbGx7K4w_ErW1lv_LBUPG6YnL4JhOYPi6o&usqp=CAU',
             4953.87,"")
             
     )
@@ -24,7 +27,7 @@ products.append(
     Product("3060874906",
             "smart tv 70 uhd samsung 4k 70au7700 processador crystal 4k tela sem limites visual livre de cabos alexa built in",
             "TV",
-            'https://images-submarino.b2w.io/Products/01/00/img/3252219/1/3252219121_1GG.jpg',
+            'https://a-static.mlcdn.com.br/618x463/smart-tv-hd-led-32-samsung-32t4300a-wi-fi-hdr-2-hdmi-1-usb/magazineluiza/225608200/68c09354fe4378c245e4e5ba097eaf4d.jpg',
             3999.99,"")
             
     )
@@ -35,7 +38,7 @@ products.append(
     Product("132966721",
             "modelador de cachos curves 1 1/4  - 31,75mm - taiff",
             "Beleza",
-            'https://images-submarino.b2w.io/Products/01/00/img/3252219/1/3252219121_1GG.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSKxRnLqayeLagXoa_iZT4xWJ5P4qupsUo0yKGZRJ-e49XDFUy8EVtjBQS9ixIkb03Pcc&usqp=CAU',
             224.90,"")
             
     )
--- gerador/gerarBase.py
+++ gerador/gerarBase.py
@@ -1,4 +1,79 @@
+from code import interact
+import json
+from random import randint
+import random
+import Lib
+import DaoProducts
+from datetime import date
 
  
 
+
  
+
+def generationSalesYear(year,data_atual ):
+     Sales=[]
+
+     #data_atual.day, data_atual.month,data_atual.year
+
+     yearActive=int(data_atual.year)
+     monthActive=int(data_atual.month)
+
+     if year!=yearActive :
+         monthActive=12
+
+
+     for salesMonth in range(monthActive):
+         month=salesMonth+1
+         for salesDay in range(29):
+             day=salesDay+1
+             amountSales=randint(1,6)
+             salesAmount=""
+             for salesAmountDay in range(amountSales):
+                 amountItens=randint(1,10)
+                 uuid=Lib.uniqueid()
+                 uuid=next(uuid)
+                 timeSales=Lib.random_date("1/1/1998 1:30 PM", "1/1/2022 12:50 AM", random.random()).split(" ")
+                 timeSales=timeSales[1]
+
+                 for itens in range(amountItens):
+                     amountItensSales=randint(1,10)
+                     product=DaoProducts.products[randint(0,len(DaoProducts.products)-1)]
+                     SalesCsv=str(uuid)+";"+str(day)+"-"+str(month)+"-"+str(year)+" "+timeSales+";"+product.description+";"+product.key+";"+product.category+";"+product.photo+";"+str(product.price)+";"+product.note+";"+str(amountItensSales)+";"+str(randint(0,25))
+                     salesAmount=salesAmount+SalesCsv+"\n"
+                     
+                     Sales.append({
+                         "uuid":uuid,"created_at":str(day)+"-"+str(month)+"-"+str(year)+" "+timeSales,
+                         "product":product.description,"product_key":product.key,
+                         "category":product.category,"photo":product.photo,
+                         "price":product.price,"note":product.note,"amount":amountItensSales,
+                         "discount":str(randint(0,25))
+                     })
+
+             print("")
+             with open("data\\base_"+str(next(Lib.uniqueid()))+".csv","a") as f:
+                     f.write(salesAmount)
+     
+
+
+
+     ##with open("data\\base_"+str(year)+".json","a") as f:
+   #      f.write(json.dumps(Sales))
+
+     #with open("data\\base_"+str(year)+".csv","a") as f:
+    #    f.write(salesAmount)
+         
+
+
+
+                    
+
+
+             
+            
+          
+year=2022
+while year !=2023:
+    generationSalesYear(year,date.today() )
+    year=year+1
+         
