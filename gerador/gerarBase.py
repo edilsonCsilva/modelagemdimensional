2d8192693076366922c7d61c982a1ab2ae7963f8
unknown <edilsonclaudinosilva@gmail.com>
2022-01-28 18:07:32 -0300

    Criação da Base de Dados De Teste parte3

--- gerador/gerarBase.py
+++ gerador/gerarBase.py
@@ -1,23 +1,41 @@
 from code import interact
 import json
+from operator import ge
 from random import randint
 import random
 import Lib
 import DaoProducts
 from datetime import date
+import csv
+import os
+
+
+ 
+def loadGeo():
+    CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+    geoPositions=[]
+    with open(CURR_DIR+"\geo.csv",'r',encoding="utf8") as f:
+        file_csv=csv.reader(f,delimiter=",")
+        for i,linha in enumerate(file_csv):
+            if i !=0:
+                geoPositions.append(str(linha))
+    
+
+    return geoPositions
+
+
 
  
 
-
- 
-
-def generationSalesYear(year,data_atual ):
+def generationSalesYear(year,data_atual,address):
      Sales=[]
 
      #data_atual.day, data_atual.month,data_atual.year
 
      yearActive=int(data_atual.year)
      monthActive=int(data_atual.month)
+     rowsAddress=len(address)
+
 
      if year!=yearActive :
          monthActive=12
@@ -27,6 +45,10 @@ def generationSalesYear(year,data_atual ):
          month=salesMonth+1
          for salesDay in range(29):
              day=salesDay+1
+             if(salesDay > 1):
+                 break
+
+                
              amountSales=randint(1,6)
              salesAmount=""
              for salesAmountDay in range(amountSales):
@@ -35,11 +57,12 @@ def generationSalesYear(year,data_atual ):
                  uuid=next(uuid)
                  timeSales=Lib.random_date("1/1/1998 1:30 PM", "1/1/2022 12:50 AM", random.random()).split(" ")
                  timeSales=timeSales[1]
+                 geo=address[randint(0,rowsAddress-1)]
 
                  for itens in range(amountItens):
                      amountItensSales=randint(1,10)
                      product=DaoProducts.products[randint(0,len(DaoProducts.products)-1)]
-                     SalesCsv=str(uuid)+";"+str(day)+"-"+str(month)+"-"+str(year)+" "+timeSales+";"+product.description+";"+product.key+";"+product.category+";"+product.photo+";"+str(product.price)+";"+product.note+";"+str(amountItensSales)+";"+str(randint(0,25))
+                     SalesCsv=str(uuid)+";"+str(day)+"-"+str(month)+"-"+str(year)+" "+timeSales+";"+product.description+";"+product.key+";"+product.category+";"+product.photo+";"+str(product.price)+";"+product.note+";"+str(amountItensSales)+";"+str(randint(0,25))+";"+str(geo)
                      salesAmount=salesAmount+SalesCsv+"\n"
                      
                      Sales.append({
@@ -47,7 +70,8 @@ def generationSalesYear(year,data_atual ):
                          "product":product.description,"product_key":product.key,
                          "category":product.category,"photo":product.photo,
                          "price":product.price,"note":product.note,"amount":amountItensSales,
-                         "discount":str(randint(0,25))
+                         "discount":str(randint(0,25)),
+                         "geo":geo
                      })
 
              print("")
@@ -71,9 +95,9 @@ def generationSalesYear(year,data_atual ):
 
              
             
-          
-year=2020
+geoMaps=loadGeo()
+year=2022
 while year !=2023:
-    generationSalesYear(year,date.today() )
+    generationSalesYear(year,date.today(),geoMaps )
     year=year+1
          
